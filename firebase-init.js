8c0c2de4dd22919222a49a588131a454cfb27e82
donovancraig85 <109382053+donovancraig85@users.noreply.github.com>
2026-03-24 10:45:44 -0400

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -1,22 +1,17 @@
-<!-- Firebase SDKs -->
-  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
-  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyD-ZVROybS5c3O6kJhe8LVcXNZ0KbYTmvg",
+  authDomain: "recipes-83727.firebaseapp.com",
+  projectId: "recipes-83727",
+  storageBucket: "recipes-83727.appspot.com",
+  messagingSenderId: "97445031584",
+  appId: "1:97445031584:web:a463b119a272531f51a3c5",
+  measurementId: "G-4LERX7EWB7"
+};
 
-  <!-- Firebase Config -->
-  <script>
-    const firebaseConfig = {
-      apiKey: "AIzaSyD-ZVROybS5c3O6kJhe8LVcXNZ0KbYTmvg",
-      authDomain: "recipes-83727.firebaseapp.com",
-      projectId: "recipes-83727",
-      storageBucket: "recipes-83727.appspot.com",
-      messagingSenderId: "97445031584",
-      appId: "1:97445031584:web:a463b119a272531f51a3c5",
-      measurementId: "G-4LERX7EWB7"
-    };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-    firebase.initializeApp(firebaseConfig);
-    const db = firebase.firestore();
-  </script>
-  <!-- App Logic -->
-  <script src="app.js"></script>
-  <script src="categories.js"></script>
+// Make Firestore globally available
+const db = firebase.firestore();
+window.db = db;
